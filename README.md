482e3a2283df44baff9d0acf001b6fc5533c6523
n2omatt <n2o.matt@gmail.com>
2016-01-20 08:55:43 -0200

    [VERSION UPDATE v0.3.1] Update the license headers. Write the README.

--- README.md
+++ README.md
@@ -1,4 +1,69 @@
-Core Minesweeper
+Core Minesweeper 
 ====
+
 Made with <3 by [Amazing Cow](http://www.amazingcow.com).
 
+<!-- ####################################################################### -->
+
+## Intro:
+This is a Game Core for Minesweeper Game.   
+You can use it as a "lib" for making Minesweeper Game in C++ concentrating 
+your efforts in making a cool game interface and letting the logic of the 
+game alone.
+
+
+<!-- ####################################################################### -->
+
+## Dependencies:
+
+This core needs to use the [CoreCoord](http://www.github.com/amazingcow/CoreCoord) 
+lib. The core itself ```#include``` the ```CoreCoord``` without any hardcoded paths
+so you must ensure that you point to the correct include dir in your compiler settings.   
+This is usually done in g++/clang++ like:
+
+```bash
+$ g++ -I/path/to/CoreCoord/ ...
+```
+
+You can find a example in the ```Makefile```.
+
+
+<!-- ####################################################################### -->
+
+## Files:
+We tried to separated the files in a very organized form.   
+Here is a list of them:
+
+* ```Block.h```
+* ```CoreMinesweeper.h```
+* ```CoreMinesweeper_Types.h```
+* ```CoreMinesweeper_Utils.h```
+* ```GameCore.h```
+
+
+<!-- ####################################################################### -->
+
+## More info:
+We tried to document the Core extensively, so we think that is pretty ease to 
+get what the Core is doing, the API calls and stuff.   
+Anyway if you didn't understand something let us know sending a mail to 
+***help_opensource@amazingcow.com***  with the subject filled with the
+name of this repo.
+
+
+<!-- ####################################################################### -->
+
+## License:
+This software is released under GPLv3.
+
+
+<!-- ####################################################################### -->
+
+## TODO:
+Check the TODO file.
+
+
+<!-- ####################################################################### -->
+
+## Others:
+Check our repos and take a look at our [open source site](http://opensource.amazingcow.com).
--- include/CoreMinesweeper_Utils.h
+++ include/CoreMinesweeper_Utils.h
@@ -2,15 +2,14 @@
 //               █      █                                                     //
 //               ████████                                                     //
 //             ██        ██                                                   //
-//            ███  █  █  ███                                                  //
-//            █ █        █ █        CoreMinesweeper_Utils.h                   //
-//             ████████████         CoreMinesweeper                           //
-//           █              █       Copyright (c) 2015 AmazingCow             //
-//          █     █    █     █      www.AmazingCow.com                        //
+//            ███  █  █  ███        CoreMinesweeper_Utils.h                   //
+//            █ █        █ █        CoreMinesweeper                           //
+//             ████████████                                                   //
+//           █              █       Copyright (c) 2015, 2016                  //
+//          █     █    █     █      AmazingCow - www.AmazingCow.com           //
 //          █     █    █     █                                                //
 //           █              █       N2OMatt - n2omatt@amazingcow.com          //
 //             ████████████         www.amazingcow.com/n2omatt                //
-//                                                                            //
 //                                                                            //
 //                  This software is licensed as GPLv3                        //
 //                 CHECK THE COPYING FILE TO MORE DETAILS                     //
@@ -27,9 +26,9 @@
 //        (See opensource.AmazingCow.com/acknowledgment.html for details).    //
 //        If you will not acknowledge, just send us a email. We'll be         //
 //        *VERY* happy to see our work being used by other people. :)         //
-//        The email is: acknowledgmentopensource@AmazingCow.com               //
+//        The email is: acknowledgment_opensource@AmazingCow.com              //
 //     3. Altered source versions must be plainly marked as such,             //
-//        and must notbe misrepresented as being the original software.       //
+//        and must not be misrepresented as being the original software.      //
 //     4. This notice may not be removed or altered from any source           //
 //        distribution.                                                       //
 //     5. Most important, you must have fun. ;)                               //
@@ -53,8 +52,8 @@
 //The core version number.
 #define COW_COREMINESWEEPER_VERSION_MAJOR    "0"
 #define COW_COREMINESWEEPER_VERSION_MINOR    "3"
-#define COW_COREMINESWEEPER_VERSION_REVISION "0"
+#define COW_COREMINESWEEPER_VERSION_REVISION "1"
 
-#define COW_MINESSWEEPERCORE_VERSION "0.3.0"
+#define COW_MINESSWEEPERCORE_VERSION "0.3.1"
 
 #endif // defined(__CoreMinesweeper_include_CoreMinesweeper_Utils_h__) //
